b96ca2e59d4b06bad76c371646126bffde8ddf8f
oskart20 <33123086+oskart20@users.noreply.github.com>
2021-03-11 15:09:21 +0100

    Update mazeScript.jl
    various improvements, most notably: now uses Set instead of Array for storing visited tiles, which improves performance tremendously
    also now throws an exception when no start or end is found in the image

--- mazeScript.jl
+++ mazeScript.jl
@@ -1,74 +1,59 @@
 #!/bin/dev/env julia
-
-import Pkg
-Pkg.add("DataStructures")
-Pkg.add("Images")
-Pkg.add("FileIO")
-Pkg.add("ImageIO")
-Pkg.add("ImageMagick")
-Pkg.add("BenchmarkTools")
+module MazeSolver
+#Pkg.add("DataStructures")
+#Pkg.add("Images")
 using Images
-using FileIO
-using ImageIO
 using DataStructures
-using BenchmarkTools
-
-if (isfile(ARGS[1]))
-	imgname = ARGS[1]
-	println(imgname)
-else 
-	println("couldn't load " * string(ARGS[1]))
-	exit()
-end
-
-img = RGB.(load(imgname))
 
 function getstart(img, h, w)
-	for j in 1:w
+	for j = 1:w
 		if img[1, j].r > 0.5
 			return (1, j)
 		end
 	end
-	return NaN
+	throw(DomainError(img, "maze must have a white pixel in first row"))
 end
 
 function getend(img, h, w)
-	for j in 1:w
+	for j = 1:w
 		if img[h, j].r > 0.5
 			return (h, j)
 		end
 	end
-	return NaN
+	throw(DomainError(img, "maze must have a white pixel in last row"))
 end
 
-@views function checkNeighbors(maze_map, index, h, w)
-	neighbors = Any[]
-	if index[1] > 1 && maze_map[index[1] - 1, index[2]] != false
+function checkNeighbors(maze_map, index, h, w)
+	neighbors = Char[]
+	if index[1] > 1 && maze_map[index[1]-1, index[2]] != false
 		push!(neighbors, 'N')
 	end
-	if index[2] < w && maze_map[index[1], index[2] + 1] != false
+	if index[2] < w && maze_map[index[1], index[2]+1] != false
 		push!(neighbors, 'E')
-	end 
-	if index[1] < h && maze_map[index[1] + 1, index[2]] != false
+	end
+	if index[1] < h && maze_map[index[1]+1, index[2]] != false
 		push!(neighbors, 'S')
-	end 
-	if index[2] > 1 && maze_map[index[1], index[2] - 1] != false
+	end
+	if index[2] > 1 && maze_map[index[1], index[2]-1] != false
 		push!(neighbors, 'W')
 	end
 	num = length(neighbors)
 	return num, neighbors
 end
 
-function checkChar(char, array); length(filter( x -> x == char, array)) >= 1; end
+function checkChar(char, array)
+	length(filter(x -> x == char, array)) >= 1
+end
 
 function getNeighbors(links, maze_map, index, h, w)
-	neighbors::Array{Tuple{Int64, Int64}, 1} = []
+	neighbors::Array{Tuple{Int64,Int64},1} = []
 	if checkChar('N', links)
 		offset = 0
-		while index[1] - (1*(offset + 1)) > 0
-			if maze_map[index[1] - (1 * (offset + 1)), index[2]] isa Bool && maze_map[index[1] - (1 * (offset + 1)), index[2]]
+		while index[1] - (1 * (offset + 1)) > 0
+			if maze_map[index[1]-(1*(offset+1)), index[2]] isa Bool &&
+					maze_map[index[1]-(1*(offset+1)), index[2]]
 				offset += 1
-			elseif !(maze_map[index[1] - (1 * (offset + 1)), index[2]] isa Bool)
+			elseif !(maze_map[index[1]-(1*(offset+1)), index[2]] isa Bool)
 				push!(neighbors, (index[1] - (1 * (offset + 1)), index[2]))
 				break
 			end
@@ -77,9 +62,10 @@ function getNeighbors(links, maze_map, index, h, w)
 	if checkChar('E', links)
 		offset = 0
 		while index[2] + (1 * (offset + 1)) <= w
-			if maze_map[index[1], index[2] + (1 * (offset + 1))] isa Bool && maze_map[index[1], index[2] + (1 * (offset + 1))]
+			if maze_map[index[1], index[2]+(1*(offset+1))] isa Bool &&
+					maze_map[index[1], index[2]+(1*(offset+1))]
 				offset += 1
-			elseif !(maze_map[index[1], index[2] + (1 * (offset + 1))] isa Bool)
+			elseif !(maze_map[index[1], index[2]+(1*(offset+1))] isa Bool)
 				push!(neighbors, (index[1], index[2] + (1 * (offset + 1))))
 				break
 			end
@@ -88,9 +74,10 @@ function getNeighbors(links, maze_map, index, h, w)
 	if checkChar('S', links)
 		offset = 0
 		while index[1] + (1 * (offset + 1)) <= h
-			if maze_map[index[1] + (1 * (offset + 1)), index[2]] isa Bool && maze_map[index[1] + (1 * (offset + 1)), index[2]]
+			if maze_map[index[1]+(1*(offset+1)), index[2]] isa Bool &&
+					maze_map[index[1]+(1*(offset+1)), index[2]]
 				offset += 1
-			elseif !(maze_map[index[1] + (1 * (offset + 1)), index[2]] isa Bool)
+			elseif !(maze_map[index[1]+(1*(offset+1)), index[2]] isa Bool)
 				push!(neighbors, (index[1] + (1 * (offset + 1)), index[2]))
 				break
 			end
@@ -99,9 +86,10 @@ function getNeighbors(links, maze_map, index, h, w)
 	if checkChar('W', links)
 		offset = 0
 		while index[2] - (1 * (offset + 1)) > 0
-			if maze_map[index[1], index[2] - (1 * (offset + 1))] isa Bool && maze_map[index[1], index[2] - (1 * (offset + 1))]
+			if maze_map[index[1], index[2]-(1*(offset+1))] isa Bool &&
+					maze_map[index[1], index[2]-(1*(offset+1))]
 				offset += 1
-			elseif !(maze_map[index[1], index[2] - (1 * (offset + 1))] isa Bool)
+			elseif !(maze_map[index[1], index[2]-(1*(offset+1))] isa Bool)
 				push!(neighbors, (index[1], index[2] - (1 * (offset + 1))))
 				break
 			end
@@ -128,7 +116,7 @@ function analyse_maze(img)
 	maze_map[Startnode[1], Startnode[2]] = 5
 	maze_map[Endnode[1], Endnode[2]] = 6
 	for i in CartesianIndices((2:h-1, 2:w-1))
-		if maze_map[i[1], i[2]] != false 
+		if maze_map[i[1], i[2]] != false
 			num, neighbors = checkNeighbors(maze_map, (i[1], i[2]), h, w)
 			if neighbors == ['N', 'S'] || neighbors == ['E', 'W']
 				maze_map[i[1], i[2]] = true
@@ -139,26 +127,30 @@ function analyse_maze(img)
 			end
 		end
 	end
-	println(counter)
+	println("	number of path tiles: " * string(counter))
 	nodes = []
 	for k in CartesianIndices(maze_map)
-		if  maze_map[k[1], k[2]] >= 2
+		if maze_map[k[1], k[2]] >= 2
 			push!(nodes, (k[1], k[2]))
 		end
 	end
-	println("number of nodes :" * string(length(nodes)))
+	println("	number of nodes :" * string(length(nodes)))
+	dict = Dict{String,Array{Tuple{Int64,Int64},1}}()
 	for a in nodes
-		maze_map[a[1], a[2]] = getNeighbors(checkNeighbors(maze_map, a, h, w)[2], maze_map, a, h, w)
+		neighbors = getNeighbors(checkNeighbors(maze_map, a, h, w)[2],
+			maze_map, a, h, w)
+		maze_map[a[1], a[2]] = neighbors
 	end
+	print("took")
 	return maze_map, nodes, Startnode, Endnode
 end
 
-@views function unfoldpath(lastnode, startnode, maze)
-	fullpath::Array{Tuple{Int64, Int64}, 1} = []
+function unfoldpath(lastnode, startnode, maze)
+	path = Array{Tuple{Int64,Int64},1}()
 	current = lastnode
 	while current != startnode
-		if ((current[1] - maze[current[1], current[2]][1]) == 0)
-			if current[2] - maze[current[1], current[2]][2] > 0
+		if ((current[1] - maze[current][1]) == 0)
+			if current[2] - maze[current][2] > 0
 				# due west
 				offset = (0, -1)
 			else
@@ -166,7 +158,7 @@ end
 				offset = (0, 1)
 			end
 		else
-			if current[1] - maze[current[1], current[2]][1] > 0
+			if current[1] - maze[current][1] > 0
 				# due north
 				offset = (-1, 0)
 			else
@@ -175,31 +167,34 @@ end
 			end
 		end
 		factor = 0
-		while (current[1] + (offset[1] * factor), current[2] + (offset[2] * factor)) != maze[current[1], current[2]]
-			push!(fullpath, (current[1] + (offset[1] * factor), current[2] + (offset[2] * factor)))
+		while (current[1] + (offset[1] * factor),
+			current[2] + (offset[2] * factor)) != maze[current]
+			push!(path, (current[1] + (offset[1] * factor),
+				current[2] + (offset[2] * factor)))
 			factor += 1
 		end
-		current = maze[current[1], current[2]]
+		current = maze[current]
 	end
-	push!(fullpath, current)
-	return fullpath
+	push!(path, current)
+	return path
 end
 
-@views function solve(maze_map, startnode::Tuple{Int64, Int64}, endnode::Tuple{Int64, Int64})
-	maze = similar(maze_map)
-	visited::Array{Tuple{Int64, Int64}, 1} = []
+function solve(maze_map, startnode::Tuple{Int64,Int64},
+			endnode::Tuple{Int64,Int64})
+	dict = Dict{Tuple{Int64,Int64},Tuple{Int64,Int64}}()
+	visited = Set{Tuple{Int64,Int64}}()
 	push!(visited, startnode)
-	q = Queue{Tuple{Int64, Int64}}()
-	maze[startnode[1], startnode[2]] = startnode
+	q = Queue{Tuple{Int64,Int64}}()
+	dict[startnode] = startnode
 	enqueue!(q, startnode)
-	@inbounds while !isempty(q)
+	while !isempty(q)
 		vertex = dequeue!(q)
 		if vertex == endnode
-			return unfoldpath(endnode, startnode, maze)
+			return unfoldpath(endnode, startnode, dict)
 		end
-		@inbounds for edge in maze_map[vertex[1], vertex[2]]
+		for edge in maze_map[vertex[1], vertex[2]]
 			if !(edge in visited)
-				maze[edge[1], edge[2]] = vertex
+				dict[edge] = vertex
 				push!(visited, edge)
 				enqueue!(q, edge)
 			end
@@ -207,16 +202,33 @@ end
 	end
 end
 
-begin
-	@time maze_map, nodes, startnode, endnode = analyse_maze(img)
-	reshape(maze_map, size(img))
-	@time result = solve(maze_map, startnode, endnode)
-
-	solvedimg = copy(img)
+function colourpath(path, painted)
+	number = 1
 	for l in result
-		solvedimg[l[1], l[2]] = RGB(1.0, 0.0, 0.0)
+		painted[l[1], l[2]] = RGB(1.0 * (number / length(result)),
+			1.0 - 1.0 * (number / length(result)),
+			0.75 - 0.5 * (number / length(result)))
+		number += 1
 	end
-	
-	save(string(split(imgname, ".")[1] * "_solved.png"), solvedimg)
-	
+	return painted
+end
+
+if (isfile(ARGS[1]))
+	filepath = ARGS[1]
+	println(filepath)
+else
+	println("couldn't load " * string(ARGS[1]))
+	exit()
+end
+
+img = RGB.(load(filepath))
+
+println("analysing maze ... ")
+@time maze_map, nodes, startnode, endnode = analyse_maze(img)
+reshape(maze_map, size(img))
+print("solved maze in ")
+@time result = solve(maze_map, startnode, endnode)
+
+save(string(split(filepath, ".")[1] * "_solved.png"), colourpath(result, copy(img)))
+println("stored maze at: " * string(split(filepath, ".")[1] * "_solved.png"))
 end
